6c4771713d94fb4d325dbe543eed3474fdbda8cb
Thomas Kellough <thomas.r.kellough@gmail.com>
2020-08-09 16:30:53 -0500

    set up main views

---
+++ How-To-Scan-A-Barcode/How-To-Scan-A-Barcode/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  How-To-Scan-A-Barcode
+//
+//  Created by Thomas Kellough on 8/8/20.
+//  Copyright © 2020 Thomas Kellough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var textLabel: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
