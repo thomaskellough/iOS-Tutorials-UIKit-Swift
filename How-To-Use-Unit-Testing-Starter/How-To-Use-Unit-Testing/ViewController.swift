8d8f4b0c68d10748cfeb1c3c3846199d4f7a6d0b
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-11-07 09:12:31 -0700

    init commit

---
+++ How-To-Use-Unit-Testing-Starter/How-To-Use-Unit-Testing/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  How-To-Use-Unit-Testing
+//
+//  Created by Thomas Kellough on 11/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var dateOfBirthTextField: UITextField!
+    @IBOutlet weak var pinNumberTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func validateTapped(_ sender: Any) {
+        
+    }
+    
+}
+
