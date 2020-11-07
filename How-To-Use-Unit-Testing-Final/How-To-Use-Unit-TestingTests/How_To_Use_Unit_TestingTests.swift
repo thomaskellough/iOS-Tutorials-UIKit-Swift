3384cc609a383f0b0e36380b164629862c58a88b
Thomas Kellough <thomas.kellough@brandtinfo.com>
2020-11-07 09:36:49 -0700

    added test target

---
+++ How-To-Use-Unit-Testing-Final/How-To-Use-Unit-TestingTests/How_To_Use_Unit_TestingTests.swift
@@ -0,0 +1,32 @@
+//
+//  How_To_Use_Unit_TestingTests.swift
+//  How-To-Use-Unit-TestingTests
+//
+//  Created by Thomas Kellough on 11/7/20.
+//
+
+import XCTest
+
+class How_To_Use_Unit_TestingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
